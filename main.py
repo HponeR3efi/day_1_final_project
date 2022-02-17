998336f5329373f77a4af4f304de5e9e57db109a
Hpone-MyatMyat <93415572+HponeR3efi@users.noreply.github.com>
2022-02-17 04:38:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Day 1 Final project ( Band Name Generator)
+
+print("Hello! Welcome to band name generator")
+city = input("What's the city name that you grew up?\n")
+pet_name = input ("What's your pet name? \n")
+print("Your Band name is " + city + " " + pet_name)
